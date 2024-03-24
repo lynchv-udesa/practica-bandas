d89636f74023494999bc5cb2acd4d4c4a3ec0237
antonelita100 <aaxelrad@udesa.edu.ar>
2024-03-24 14:30:01 -0300

    buscador de genero

--- bandas/controllers/bandasController.js
+++ bandas/controllers/bandasController.js
@@ -1,46 +1,61 @@
 let db = require('../db/index')
 
 const bandasController = {
+    searchB: function(req,res) {
+        const genero = req.query.genero; 
+        if (genero) {
+            return res.redirect(`/bandas/generos/${genero}`)
+        } else {
+            return res.render("generos", {
+                mensaje: "Busca tu género favorito"
+            });
+    }},
     index: function(req,res){
         return res.render('index',{
             index: db.lista,
-            mensaje: 'todas las bandas'
+        });
+    },
+
+    banda: function(req,res){
+        return res.render('bandas',{
+            index: db.lista,
+            mensaje: 'Todas las bandas'
         });
     },
 
     iD: function(req, res) {
         let bandaId = req.params.id;
         let resultado = [];
-        for (let i =0; i < db.lista.length; i++ ) {
+        for (let i = 0; i < db.lista.length; i++ ) {
             if(bandaId == db.lista[i].id) {
                 resultado.push(db.lista[i])
             }
         }
         if(resultado.length == 0) {
-            return res.render('index', {
+            return res.render('detalleBanda', {
                 index: resultado,
                 mensaje: 'no tenemos a la banda con el id ' + bandaId,
             })
         }
         else{
-            return res.render('index', {
+            return res.render('detalleBanda', {
                 index: resultado,
                 mensaje: 'Banda con id ' + bandaId,
             })
         } },
 
         genero: function(req, res) {
-            let bandaGenero = req.params.id;
+            let bandaGenero = req.params.genero;
             let resultado = [];
             for (let i =0; i < db.lista.length; i++ ) {
-                if(bandaGenero == db.lista[i].id) {
+                if(bandaGenero == db.lista[i].genero) {
                     resultado.push(db.lista[i])
                 }
             }
             if(resultado.length == 0) {
-                return res.render('bandas', {
+                return res.render('generos', {
                     index: resultado,
-                    mensaje: 'no tenemos bandas del genero ' + bandaGenero,
+                    mensaje: 'No tenemos bandas del género ' + bandaGenero,
                 })
             }
             else{
--- bandas/routes/bandas.js
+++ bandas/routes/bandas.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const bandasController = require('../controllers/bandasController');
 
 router.get('/', bandasController.index);
+router.get("/bandas", bandasController.banda)
+router.get("/bandas/generos", bandasController.searchB)
+router.get('/bandas/generos/:genero', bandasController.genero);
 router.get('/bandas/:id', bandasController.iD);
-router.get('/bandas/:genero', bandasController.genero);
 
 module.exports = router;
